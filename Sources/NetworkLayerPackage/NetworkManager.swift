121da44ed3c947c6e5de4ab6914d754d8d4c9dfa
mertdemirtas <m.demirtas99@icloud.com>
2022-09-19 20:13:18 +0300

    ..

--- Sources/NetworkLayerPackage/NetworkManager.swift
+++ Sources/NetworkLayerPackage/NetworkManager.swift
@@ -10,12 +10,13 @@ import Foundation
 open class NetworkManager {
     public static let shared = NetworkManager()
     
-    var networkStateClosure: ((NetworkStates) -> Void)?
+    public var networkState: ((NetworkStates) -> Void)?
     
     private init() {
     }
     
     open func fetchData<T: Decodable>(from response: Response, completionHandler: @escaping (Result<T, NetworkErrors>) -> Void) {
+        networkState?(.processing)
         
         // MARK: URL
         guard let urlString = response.urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
@@ -35,22 +36,26 @@ open class NetworkManager {
             }
         }
         
-        let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
+        let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: { [weak self] data, response, error in
 
             if let statusCode = (response as? HTTPURLResponse)?.statusCode , !(200..<300 ~= statusCode) {
-                completionHandler(.failure(NetworkErrors(statusCode: statusCode)))
+                let error = NetworkErrors(statusCode: statusCode)
+                completionHandler(.failure(error))
                 return
             }
             guard let data = data, error == nil else {
+                self?.networkState?(.done)
                 completionHandler(.failure(.unknown))
                 return
             }
             
             do {
                 let decodedData = try JSONDecoder().decode(T.self, from: data)
+                self?.networkState?(.done)
                 completionHandler(.success(decodedData))
             }
             catch {
+                self?.networkState?(.clientError)
                 completionHandler(.failure(.decodeError))
             }
         })
