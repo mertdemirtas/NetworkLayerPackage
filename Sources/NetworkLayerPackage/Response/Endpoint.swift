aee91b2993926da388e6c58c6c082184be7cb5cc
mertdemirtas <m.demirtas99@icloud.com>
2022-09-23 13:20:52 +0300

    ..

--- Sources/NetworkLayerPackage/Response/Endpoint.swift
+++ Sources/NetworkLayerPackage/Response/Endpoint.swift
@@ -7,20 +7,13 @@
 
 import Foundation
 
-open class Endpoint {
-    public var networkConstants : NetworkConstants
-    public var httpMethod: HTTPMethods
-    public var path: String?
-    public var headers: [String: String]?
-    public var body: [String: Any]?
+public protocol Endpoint {
+    var networkConstants : NetworkConstants { get }
+    var httpMethod: HTTPMethods { get }
+    var path: String? { get }
+    var hbeaders: [String: String]? { get }
+    var body: [String: Any]? { get }
     
-    public init(networkConstants: NetworkConstants, httpMethod: HTTPMethods, path: String? = nil, headers: [String:String]? = nil, body: [String: Any]? = nil) {
-        self.networkConstants = networkConstants
-        self.httpMethod = httpMethod
-        self.path = path
-        self.headers = headers
-        self.body = body
-    }
 }
 
 extension Endpoint {
